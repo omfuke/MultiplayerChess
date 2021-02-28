4282d8ec721ab7a05091e10f1f533cb7be87332e
omfuke <46218908+omfuke@users.noreply.github.com>
2021-02-28 22:42:05 +0530

    added pieces

--- client/src/Board/Board.js
+++ client/src/Board/Board.js
@@ -4,14 +4,50 @@ import Tyle from "./Tyle/Tyle";
 
 function Board() {
   const [board, setBoard] = useState([
+    [
+      { name: "rook", color: "black" },
+      { name: "knight", color: "black" },
+      { name: "bishop", color: "black" },
+      { name: "king", color: "black" },
+      { name: "queen", color: "black" },
+      { name: "bishop", color: "black" },
+      { name: "knight", color: "black" },
+      { name: "rook", color: "black" },
+    ],
+    [
+      { name: "pawn", color: "black" },
+      { name: "pawn", color: "black" },
+      { name: "pawn", color: "black" },
+      { name: "pawn", color: "black" },
+      { name: "pawn", color: "black" },
+      { name: "pawn", color: "black" },
+      { name: "pawn", color: "black" },
+      { name: "pawn", color: "black" },
+    ],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
+    [
+      { name: "pawn", color: "white" },
+      { name: "pawn", color: "white" },
+      { name: "pawn", color: "white" },
+      { name: "pawn", color: "white" },
+      { name: "pawn", color: "white" },
+      { name: "pawn", color: "white" },
+      { name: "pawn", color: "white" },
+      { name: "pawn", color: "white" },
+    ],
+    [
+      { name: "rook", color: "white" },
+      { name: "knight", color: "white" },
+      { name: "bishop", color: "white" },
+      { name: "king", color: "white" },
+      { name: "queen", color: "white" },
+      { name: "bishop", color: "white" },
+      { name: "knight", color: "white" },
+      { name: "rook", color: "white" },
+    ],
   ]);
   let tyle;
   return (
@@ -29,21 +65,21 @@ function Board() {
           console.log(index);
 
           return i.map((j, ind) => {
-            if (index % 2 == 0) {
-              if (ind % 2 == 0) {
+            if (index % 2 === 0) {
+              if (ind % 2 === 0) {
                 tyle = true;
               } else {
                 tyle = false;
               }
             } else {
-              if (ind % 2 == 0) {
+              if (ind % 2 === 0) {
                 tyle = false;
               } else {
                 tyle = true;
               }
             }
 
-            return <Tyle tyle={tyle}></Tyle>;
+            return <Tyle tyle={tyle} detail={j}></Tyle>;
           });
         })}
       </div>
--- client/src/Board/Tyle/Tyle.js
+++ client/src/Board/Tyle/Tyle.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./Tyle.css";
 
-function Tyle({ tyle }) {
+function Tyle({ tyle, detail }) {
   return (
     <div
       className="Tyle"
-      style={{ backgroundColor: tyle ? "#D68910" : "#F6DDCC" }}
-    ></div>
+      style={{ backgroundColor: tyle ? "#D68910" : "#ffcc99" }}
+    >
+      <div
+        style={{
+          width: "50px",
+          height: "50px",
+          cursor: "pointer",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "7.5px",
+        }}
+      >
+        <i
+          style={{
+            color: detail && `${detail.color}`,
+          }}
+          className={detail && `fas fa-chess-${detail.name} fa-2x`}
+        ></i>
+      </div>
+    </div>
   );
 }
 
